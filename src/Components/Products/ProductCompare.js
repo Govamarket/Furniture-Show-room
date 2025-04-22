8c670b3fcca18f313c83e1659793aa6b076c3dae
Govamarket <govaway@gmail.com>
2025-04-22 20:54:34 +0100

    initial commit

--- src/Components/Products/ProductCompare.js
+++ src/Components/Products/ProductCompare.js
@@ -1,9 +1,23 @@
 import React from "react";
 import ProductImg from "./../../Asset/productc.png";
 import logoManu from "./../../Asset/MeuLogos.png";
+import CenterSofa from "./../../Asset/center.png";
+// import BlackSofa from "./../../Asset/inlinef.png";
 
 const ProductComparison = () => {
-  console.log(ProductImg);
+  const productItems = [
+    {
+      imgs: <img src={CenterSofa} />,
+      text: "Asgaard Sofa",
+      price: "Rs. 250,000.00",
+    },
+
+    {
+      imgs: <img src={CenterSofa} />,
+      text: "Asgaard Sofa",
+      price: "Rs. 250,000.00",
+    },
+  ];
   return (
     <div>
       <div
@@ -33,6 +47,15 @@ const ProductComparison = () => {
           </div>
         </div>
       </div>
+      <section>
+        {productItems.map((item, index) => (
+          <div key={index} className="flex justify-between">
+            <div className="size-60">{item.imgs}</div>
+            <div>{item.text}</div>
+            <div>{item.price}</div>
+          </div>
+        ))}
+      </section>
     </div>
   );
 };
